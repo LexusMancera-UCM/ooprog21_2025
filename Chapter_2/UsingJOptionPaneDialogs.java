48eaf398d203f758eb02c56dfbdfda698b1775c2
LexusMancera <lexmn47@gmail.com>
2025-09-22 09:01:05 +0800

    Update UsingJOptionPaneDialogs.java

--- Chapter_2/UsingJOptionPaneDialogs.java
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -1,1 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class UsingJOptionPaneDialogs {
+    public static void main(String[] args) {
+        while (true) {
+            String name = JOptionPane.showInputDialog(null, "What is your name?", "Input", JOptionPane.QUESTION_MESSAGE);
+
+            if (name == null) {
+                break;
+            }
+
+            int response = JOptionPane.showConfirmDialog(
+                null,
+                "Do you want your name to be shown?",
+                "Confirmation",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+            );
+
+            if (response == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Hello, " + name, "Message", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            }
+        }
+    }
+}
 
