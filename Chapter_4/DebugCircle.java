8d63c8ecd8da84f2ce51e2fdae15307138ea2682
LexusMancera <lexmn47@gmail.com>
2025-09-24 08:35:44 +0800

    Update DebugCircle.java

--- Chapter_4/DebugCircle.java
+++ Chapter_4/DebugCircle.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
+
+public class DebugCircle {
+    private int radius;
+    private int diameter;
+    private final double PI = 3.14159;
+    private double area;
+
+    public DebugCircle(int r) {
+        if (r < 0) {
+            System.out.println("Radius cannot be negative. Setting to 0.");
+            r = 0;
+        }
+        radius = r;
+        diameter = 2 * r;
+        area = PI * r * r;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter radius of the circle: ");
+        int r = scanner.nextInt();
+
+        DebugCircle circle = new DebugCircle(r);
+
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Diameter: " + circle.getDiameter());
+        System.out.println("Area: " + circle.getArea());
+    }
+}
 
